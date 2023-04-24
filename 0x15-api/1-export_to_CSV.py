f8b3d3d5fd42beec1b2dc2530630fcbe58f6dd3a
Chebet Kamara <chebetkamara@gmail.com>
2023-04-24 12:09:56 -0700

    Fixing errors

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,19 +12,18 @@ if __name__ == "__main__":
     """
     do the following
     """
-    api_url_user = "https://jsonplaceholder.typicode.com/users"
-    api_url_todo = "https://jsonplaceholder.typicode.com/todos"
+    api_url = "https://jsonplaceholder.typicode.com/"
+    # api_url_todo = "https://jsonplaceholder.typicode.com/todos"
     param_todo = {'userId': sys.argv[1]}
 
     user_id = sys.argv[1]
     response_user = requests.get(
-            api_url_user + '/{}'.format(sys.argv[1])).json()
+            api_url + 'users/{}'.format(sys.argv[1])).json()
     username = response_user.get('username')
-    response_todo = requests.get(api_url_todo, param_todo).json()
+    response_todo = requests.get(api_url + "todos", param_todo).json()
 
     with open("{}.csv".format(user_id), 'w', newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         [writer.writerow(
             [user_id, username, r.get("completed"), r.get(
                 "title")]) for r in response_todo]
-        
