5939151681e009c1a4b3220b8bc8484f70da86de
Chebet Kamara <chebetkamara@gmail.com>
2023-05-10 07:52:51 -0700

    Fixing PEP8 errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,13 @@ return number of subs for a given subreddit
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """returns as mentioned"""
     api_url = "https://api.reddit.com/r/{}/about".format(subreddit)
-    headers = {'User-Agent': 'Windows/11'}    
+    headers = {'User-Agent': 'Windows/11'}
     response = requests.get(api_url, allow_redirects=False, headers=headers)
-    #print(response)
+    # print(response)
     if response.status_code != 200:
         return (0)
 
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,13 +6,15 @@ prints the titles of the first 10 hot posts listed for a given subreddit
 
 import requests
 
+
 def top_ten(subreddit):
     """return as menstioned"""
     api_url = "https://api.reddit.com/r/{}?sort=hot".format(subreddit)
     headers = {'User-Agent': 'Windows/11'}
     params = {'limit': 10}
-    response = requests.get(api_url, allow_redirects=False, headers=headers, params=params)
-    #print(response)
+    response = requests.get(
+            api_url, allow_redirects=False, headers=headers, params=params)
+    # print(response)
     if response.status_code != 200:
         return (0)
     response = response.json()
