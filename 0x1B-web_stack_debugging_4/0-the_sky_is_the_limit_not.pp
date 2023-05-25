3ee4e4a3375d7cc33139f9bb6221b3b1d7f5d1b0
tkamara <107914724+tkamara@users.noreply.github.com>
2023-05-25 12:52:56 +0300

    Delete 0x1B-web_stack_debugging_4 directory

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++
@@ -1,9 +0,0 @@
-# ensuring Nginx requests don't fail
-exec {'correction':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/bin',
-}
-
-exec {'service nginx restart':
-  path => '/usr/bin'
-}
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++
@@ -1,8 +0,0 @@
-# changing limits
-exec {'sed -i "s/20/10000" /etc/default/nginx':
-  path => '/bin',
-}
-
-exec {'sed -i "s/50/10000" /etc/default/nginx':
-  path => '/bin',
-}
