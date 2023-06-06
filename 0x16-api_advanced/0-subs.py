5e4f143b36d57770bc200a84e1d3dec7c5455409
Chebet Kamara <chebetkamara@gmail.com>
2023-06-06 07:20:41 -0700

    Correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns no. of subs for a given subreddit"""
-    api_url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Windows/11"}
     response = requests.get(api_url, headers=headers, allow_redirects=False)
-    response_json = response.json()
     if response.status_code != 200:
         return (0)
-    if 'data' in response_json:
-        return (response_json.get('data').get('subscribers'))
+    return (response.json().get('data').get('subscribers'))
