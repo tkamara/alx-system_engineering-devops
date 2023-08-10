d107b4b25b6082c385d46faf0f1fa1d34d53b55a
Chebet Kamara <chebetkamara@gmail.com>
2023-08-10 01:28:35 -0700

    API advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" getting number of subs"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns no. of subs for a given subreddit"""
+    api_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Windows/11"}
+    response = requests.get(api_url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return (0)
+    return (response.json().get('data').get('subscribers'))
